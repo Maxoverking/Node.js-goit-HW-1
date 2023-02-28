05abb3d8e56c491d28a3a7bdf9121c5544b68a4e
Maxoverking <avemax7@gmail.com>
2023-02-28 10:37:14 +0100

    first commit

---
+++ contacts.js
@@ -0,0 +1,17 @@
+// const contactsPath = ;
+
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+const fs = require("fs").promises;
+console.log(2345566575);
